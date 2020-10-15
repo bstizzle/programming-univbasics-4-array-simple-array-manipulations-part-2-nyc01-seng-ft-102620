41c2fe0a4e3916c10e5a100945b7934f2b975608
Benjamin Stein <whereyouben@gmail.com>
2020-10-15 04:45:35 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -5,3 +5,19 @@ end
 def using_insert(array, element)
   array.insert(4, element)
 end 
+
+def using_uniq(array)
+  array.uniq 
+end 
+
+def using_flatten(array)
+  array.flatten 
+end 
+
+def using_delete(array, element)
+  array.delete(element)
+end 
+
+def using_delete_at(array, int)
+  array.delete_at(int)
+end 
